24d27708e5658dac39bef3a3c41dbf9241252408
Utsav1510 <jiutsav8@gmail.com>
2023-05-09 09:28:44 +0530

    Initial Commit

---
+++ IOSMoEngage/SecondViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SecondViewController.swift
+//  IOSMoEngage
+//
+//  Created by Utsav Punia on 09/05/23.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+       
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func backbutton(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    
+    
+  
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
