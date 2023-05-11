2fa9c0f6975a91f4e765d88ee2fa60919881839e
Utsav1510 <jiutsav8@gmail.com>
2023-05-11 14:00:09 +0530

    Merge remote-tracking branch 'refs/remotes/origin1/main'

--- IOSMoEngage/ThirdViewController.swift
+++ IOSMoEngage/ThirdViewController.swift
@@ -23,27 +23,88 @@ class ThirdViewController: UIViewController {
     
     @IBOutlet weak var firstname: UITextField!
     
+    @IBOutlet weak var adidasbef: UITextField!
+    
+    
+    @IBOutlet weak var Rate: UITextField!
+    
+    var count: Int = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        dob.locale = .current
+        dob.date = Date();
+        
         firstname.delegate=self //send msgs to self object
         lastname.delegate=self
         email.delegate=self
         gender.delegate=self
         phone.delegate=self
+        dob.addTarget(self, action: #selector(dateselected), for: .valueChanged)
+        Rate.delegate=self
         
-        
+        adidasbef.delegate = self
+       
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func dob(_ sender: Any) {
+        
+    }
+    var var1: Date? = Date ();
+    @objc
+    func dateselected(){
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        print("hello")
+        let date = formatter.string(from: dob.date)
+            print(date)
+         
+        var1=formatter.date(from: date)
+        
+    }
+    
+    
     @IBAction func signupbutton(_ sender: Any) {
+        
+       
+            
+      
+        
         MoEngageSDKAnalytics.sharedInstance.setFirstName(firstname.text!)
         MoEngageSDKAnalytics.sharedInstance.setLastName(lastname.text!)
         MoEngageSDKAnalytics.sharedInstance.setEmailID(email.text!)
         MoEngageSDKAnalytics.sharedInstance.setMobileNumber(phone.text!)
         MoEngageSDKAnalytics.sharedInstance.setUniqueID(email.text!)
-
+        MoEngageSDKAnalytics.sharedInstance.setDateOfBirth(var1!)
+        
+        MoEngageSDKAnalytics.sharedInstance.setUserAttribute(Int(Rate.text!), withAttributeName: "Rate")
+        MoEngageSDKAnalytics.sharedInstance.setUserAttribute( count , withAttributeName: "ButtonClicked")
+        count=count+1
+        print(count)
+        
+        switch gender.text{
+        case "male":
+            MoEngageSDKAnalytics.sharedInstance.setGender(.male)
+        case "female":
+            MoEngageSDKAnalytics.sharedInstance.setGender(.female)
+        case .none:
+            print("hello")
+        case .some(_):
+            print("hello")
+        }
+        
+        if adidasbef.text! == "yes"{
+            MoEngageSDKAnalytics.sharedInstance.setUserAttribute(true, withAttributeName: " Prev offline/online shopping")
+            
+        }
+        else{
+            MoEngageSDKAnalytics.sharedInstance.setUserAttribute(false, withAttributeName: " Prev offline/online shopping")
+            
+        }
+        
 
         
     }
